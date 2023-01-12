f12bfca6bae5d180e7623e19d461658011701609
totalminibmw <totalminibmw.chad@att.net>
2023-01-12 13:23:35 -0500

    Added GUI to open files and changed name for Main.py to open_file.py

---
+++ open_file.py
@@ -0,0 +1,49 @@
+from tkinter import *
+from tkinter import filedialog
+
+def openFile():
+    tech_proc = filedialog.askopenfilename(initialdir="C:\\tmb_ammended_projects\\Processes\\Technician Processes",
+                                          title="Open file okay?",
+                                          filetypes= (("text files","*.txt, *.docx"),
+                                          ("all files","*.*")))
+    file = open(tech_proc,'r')
+    print(file.read())
+    file.close()
+
+window = Tk()
+button = Button(text="Tech Procedures",command=openFile)
+button.pack()
+
+def openFile():
+    sa_proc = filedialog.askopenfilename(initialdir="C:\\tmb_ammended_projects\\Processes\\Service advisor Processes",
+                                          title="Open file okay?",
+                                          filetypes= (("text files","*.txt, *.docx"),
+                                          ("all files","*.*")))
+    file = open(sa_proc,'r')
+    print(file.read())
+    file.close()
+
+button = Button(text="Service Advisor Procedures",command=openFile)
+button.pack()
+
+def openFile():
+    sa_proc = filedialog.askopenfilename(initialdir="C:\\tmb_ammended_projects\\Processes\\TMB Posted policy",
+                                          title="Open file okay?",
+                                          filetypes= (("text files","*.txt, *.docx"),
+                                          ("all files","*.*")))
+    file = open(sa_proc,'r')
+    print(file.read())
+    file.close()
+
+button = Button(text="TMB Posted Policy",command=openFile)
+button.pack()
+
+
+
+
+
+
+
+
+
+window.mainloop()
